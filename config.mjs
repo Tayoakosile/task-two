4bdf6bd83e8ee53bf64607227e1a709620e8bb12
Tayo Akosile <oluwatayoakosile@gmail.com>
2023-09-12 08:37:23 +0100

    feat:removed openapi docs

--- config.mjs
+++ config.mjs
@@ -1,6 +1,6 @@
 import fastify from "fastify";
 
-import FastOpenAPI from "fastify-openapi-docs";
+// import FastOpenAPI from "fastify-openapi-docs";
 
 import fastifyEnv from "@fastify/env";
 import mongoose from "mongoose";
@@ -11,42 +11,42 @@ export const PORT = process.env.PORT || 4000;
 const app = fastify({ logger: true });
 app.register(fastifyFormbody);
 
-await app.register(FastOpenAPI, {
-  openapi: {
-    openapi: "3.0.3",
-    info: {
-      title: "PERSON API",
-      description: "Hello world",
-      version: "1.0.0",
-      license: "MIT",
-    },
-    contact: {
-      name: "Tayo Akosile",
-      url: "https://github.com/tayoakosile",
-      email: "tayoakosile@gmail.com",
-    },
-    host: "vercel",
-    schemes: ["https"],
+// await app.register(FastOpenAPI, {
+//   openapi: {
+//     openapi: "3.0.3",
+//     info: {
+//       title: "PERSON API",
+//       description: "Hello world",
+//       version: "1.0.0",
+//       license: "MIT",
+//     },
+//     contact: {
+//       name: "Tayo Akosile",
+//       url: "https://github.com/tayoakosile",
+//       email: "tayoakosile@gmail.com",
+//     },
+//     host: "vercel",
+//     schemes: ["https"],
 
-    servers: [{ url: "https://example.com", description: "Production Server" }],
+//     servers: [{ url: "https://example.com", description: "Production Server" }],
 
-    // tags: [
-    //   { name: "Default", description: "Default" },
-    //   { name: "Fetch All Users", description: "userss" },
-    // ],
-  },
+//     // tags: [
+//     //   { name: "Default", description: "Default" },
+//     //   { name: "Fetch All Users", description: "userss" },
+//     // ],
+//   },
 
-  definitions: {
-    Person: {
-      type: "object",
-      required: ["name"],
-      properties: {
-        id: { type: "string" },
-        name: { type: "string" },
-      },
-    },
-  },
-});
+//   definitions: {
+//     Person: {
+//       type: "object",
+//       required: ["name"],
+//       properties: {
+//         id: { type: "string" },
+//         name: { type: "string" },
+//       },
+//     },
+//   },
+// });
 
 (async () => {
   try {
@@ -69,33 +69,33 @@ await app.register(FastOpenAPI, {
     app.log.error(err);
   }
 })();
-app.addSchema({
-  type: "object",
-  $id: "request",
-  description: "The request payload",
-  properties: {
-    id: {
-      type: "string",
-      description: "The operation id",
-      pattern: "^.+$",
-    },
-  },
-  required: ["id"],
-  additionalProperties: false,
-});
+// app.addSchema({
+//   type: "object",
+//   $id: "request",
+//   description: "The request payload",
+//   properties: {
+//     id: {
+//       type: "string",
+//       description: "The operation id",
+//       pattern: "^.+$",
+//     },
+//   },
+//   required: ["id"],
+//   additionalProperties: false,
+// });
 
-app.addSchema({
-  type: "object",
-  $id: "response",
-  description: "The response payload",
-  properties: {
-    ok: {
-      type: "boolean",
-      description: "The operation response",
-    },
-  },
-  required: ["ok"],
-  additionalProperties: false,
-});
+// app.addSchema({
+//   type: "object",
+//   $id: "response",
+//   description: "The response payload",
+//   properties: {
+//     ok: {
+//       type: "boolean",
+//       description: "The operation response",
+//     },
+//   },
+//   required: ["ok"],
+//   additionalProperties: false,
+// });
 
 export default app;
