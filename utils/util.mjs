54a2aba51e3e2bae5ce507c83e830443eeab21e9
Tayo Akosile <oluwatayoakosile@gmail.com>
2023-09-13 14:46:46 +0100

    feat:updated index.js

--- utils/util.mjs
+++ utils/util.mjs
@@ -36,7 +36,7 @@ export const getUserList = async (request, reply) => {
 
   const filteredList = persons?.map((person) => filterResByNameAndId(person));
 
-  reply.code(200).send(filteredList);
+return  reply.code(200).send(filteredList);
 };
 
 export const filterResByNameAndId = (person) => {
